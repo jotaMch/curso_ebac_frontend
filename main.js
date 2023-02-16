55cacf2939ab7f77ab28a7aeab55d21c17eb0f29
Jânderson Machado <jandersonmachado090@gmail.com>
2023-02-16 15:51:42 -0300

    new file

---
+++ main.js
@@ -0,0 +1,35 @@
+const form = document.getElementById('form-deposito');
+const numeroConta = document.getElementById('numero-conta');
+const valorDeposito = document.getElementById('valor-deposito');
+
+function validaNumeros() {
+    const numeroConta = document.getElementById('numero-conta').value;
+    const valorDeposito = document.getElementById('valor-deposito').value;
+
+    if (numeroConta >= valorDeposito) {
+    return false;
+    }
+    return true; 
+}
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    const numeroConta = document.getElementById('numero-conta');
+    const valorDeposito = document.getElementById('valor-deposito');
+    const mensagemSucesso = `Deposito feito com sucesso`
+
+    formEValido = validaNumeros(valorDeposito.value)
+    if(formEValido) {
+        const containerMensagemSucesso = document.querySelector('.success-message');
+        containerMensagemSucesso.innerHTML = mensagemSucesso;
+        containerMensagemSucesso.style.display = 'block';
+
+    } else {
+        document.querySelector('.error-message').style.display = 'block';
+    }
+})
+
+console.log(form);
+
+
+
